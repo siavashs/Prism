91a5df1b6f8a5db27fe3a02b19e0c28614fd9973
Siavash Safi <s.safisamghabadi@catawiki.nl>
2017-08-24 17:26:29 +0200

    Add new syntax for enabled Prism plugins
    New syntaxes:
      * ln or linenumber
      * hl or highlight
      * cmd

--- src/main/java/com/catawiki/jira/prism/Code.java
+++ src/main/java/com/catawiki/jira/prism/Code.java
@@ -36,12 +36,46 @@ public class Code extends BaseMacro {
     public String execute(Map<String, Object> parameters, String body, RenderContext renderContext) throws MacroException {
 
         ImmutableMap.Builder<String, Object> templateParams = ImmutableMap.builder();
+
+        // Code body
         templateParams.put("content", body);
-        String language = "ruby";
-        if (parameters.get("0") != null) {
-          language = (String) parameters.get("0");
+
+        // line numbers (optional)
+        if (parameters.containsValue("linenumbers") || parameters.containsValue("ln")) {
+            templateParams.put("linenumbers", true);
         }
-        templateParams.put("language", language);
+
+        for (Map.Entry<String, Object> entry : parameters.entrySet()) {
+            switch (entry.getKey()) {
+                case "highlight":
+                case "hl":
+                    templateParams.put("highlight", (String) entry.getValue());
+                    break;
+                case "commandline":
+                case "cmd":
+                    String cmd = (String) entry.getValue();
+                    String parts[] = cmd.split(">");
+                    String prompt[] = parts[0].split("@");
+                    String output = parts[1];
+                    String user = prompt[0];
+                    String host = prompt[1];
+                    templateParams.put("commandline", true);
+                    templateParams.put("user", user);
+                    templateParams.put("host", host);
+                    templateParams.put("output", output);
+                    break;
+                default:
+            }
+        }
+
+        if (parameters.containsKey("0")) {
+            String language = "ruby";
+            String parameter = (String) parameters.get("0");
+            if (checkLanguage(parameter)) {
+                language = parameter;
+            }
+            templateParams.put("language", language);
+        }
 
         try {
             return this.soyTemplateRenderer.render(
@@ -50,6 +84,136 @@ public class Code extends BaseMacro {
                     templateParams.build());
         } catch (SoyException e) {
             return String.format(FALLBACK_RENDER_OUTPUT, body);
+        }
+    }
+
+    private Boolean checkLanguage(String language) {
+        switch (language) {
+            case "abap":
+            case "actionscript":
+            case "ada":
+            case "apacheconf":
+            case "apl":
+            case "applescript":
+            case "asciidoc":
+            case "aspnet":
+            case "autohotkey":
+            case "autoit":
+            case "bash":
+            case "basic":
+            case "batch":
+            case "bison":
+            case "brainfuck":
+            case "bro":
+            case "c":
+            case "clike":
+            case "coffeescript":
+            case "cpp":
+            case "crystal":
+            case "csharp":
+            case "css":
+            case "css-extras":
+            case "d":
+            case "dart":
+            case "diff":
+            case "django":
+            case "docker":
+            case "eiffel":
+            case "elixir":
+            case "erlang":
+            case "fortran":
+            case "fsharp":
+            case "gherkin":
+            case "git":
+            case "glsl":
+            case "go":
+            case "graphql":
+            case "groovy":
+            case "haml":
+            case "handlebars":
+            case "haskell":
+            case "haxe":
+            case "http":
+            case "icon":
+            case "inform7":
+            case "ini":
+            case "j":
+            case "jade":
+            case "java":
+            case "javascript":
+            case "jolie":
+            case "json":
+            case "jsx":
+            case "julia":
+            case "keyman":
+            case "kotlin":
+            case "latex":
+            case "less":
+            case "livescript":
+            case "lolcode":
+            case "lua":
+            case "makefile":
+            case "markdown":
+            case "markup":
+            case "matlab":
+            case "mel":
+            case "mizar":
+            case "monkey":
+            case "nasm":
+            case "nginx":
+            case "nim":
+            case "nix":
+            case "nsis":
+            case "objectivec":
+            case "ocaml":
+            case "oz":
+            case "parigp":
+            case "parser":
+            case "pascal":
+            case "perl":
+            case "php":
+            case "php-extras":
+            case "powershell":
+            case "processing":
+            case "prolog":
+            case "properties":
+            case "protobuf":
+            case "puppet":
+            case "pure":
+            case "python":
+            case "q":
+            case "qore":
+            case "r":
+            case "reason":
+            case "rest":
+            case "rip":
+            case "roboconf":
+            case "ruby":
+            case "rust":
+            case "sas":
+            case "sass":
+            case "scala":
+            case "scheme":
+            case "scss":
+            case "smalltalk":
+            case "smarty":
+            case "sql":
+            case "stylus":
+            case "swift":
+            case "tcl":
+            case "textile":
+            case "twig":
+            case "typescript":
+            case "vbnet":
+            case "verilog":
+            case "vhdl":
+            case "vim":
+            case "wiki":
+            case "xojo":
+            case "yaml":
+                return true;
+            default:
+                return false;
         }
     }
 }
