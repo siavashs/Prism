2591ec0dc078f683e1878dcdb8908b5dd8c49943
Siavash Safi <siavash.safi@gmail.com>
2019-05-01 20:41:30 +0200

    Improve JS event handling
    - Highlight elements on `NEW_CONTENT_ADDED` conditionally
    - Highlight all on `ViewIssueTabs.onTabReady`, fixes #20

--- src/main/resources/js/prism-init.js
+++ src/main/resources/js/prism-init.js
@@ -1,7 +1,16 @@
-AJS.toInit(function(){
-  Prism.highlightAll();
+// Highlight new content
+JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (e, context, reason) {
+  switch(reason) {
+    case "pageLoad":
+    case "panelRefreshed":
+      Prism.highlightAllUnder(e.target);
+      break;
+    default:
+      // console.log("Prism: ignored content,", reason)
+  }
 });
 
-JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (e, context, reason) {
+// Highlight content in issue tabs
+JIRA.ViewIssueTabs.onTabReady(function() {
   Prism.highlightAll();
 });
