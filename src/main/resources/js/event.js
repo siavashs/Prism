82b57b9888d0d9d2a6ac4c1f5a5a8e887a3bb135
Siavash Safi <siavash.safi@gmail.com>
2020-10-24 19:59:09 +0200

    Improve event handling using timeouts, add logs
    Improve event handling by introducing timeouts, this fixes issues with JIRA
    firing the events too early.
    Add logs for captured or ignored events and time measurements.

--- src/main/resources/js/event.js
+++ src/main/resources/js/event.js
@@ -1,16 +1,29 @@
-// Highlight new content
+// Highlight new content dynamically for older versions of JIRA
 JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (e, context, reason) {
   switch(reason) {
     case "pageLoad":
     case "panelRefreshed":
-      Prism.highlightAllUnder(e.target);
+      console.debug("Prism captured event: JIRA.Events.NEW_CONTENT_ADDED, reason:", reason);
+      setTimeout(function(){
+        console.time('Prism.highlightAllUnder');
+        Prism.highlightAllUnder(e.target);
+        console.timeEnd('Prism.highlightAllUnder');
+      }, 1000);
       break;
     default:
-      // console.log("Prism: ignored content,", reason)
+      console.debug("Prism ignored reason:", reason)
   }
 });
 
-// Highlight content in issue tabs
+// Highlight (new) content in issue tabs
 JIRA.ViewIssueTabs.onTabReady(function() {
-  Prism.highlightAll();
+  /* Set a timeout since JIRA navigator fires this event too early,
+     resulting in code style to be lost!
+  */
+  console.debug("Prism captured event: JIRA.ViewIssueTabs.onTabReady");
+  setTimeout(function(){
+    console.time('Prism.highlightAll');
+    Prism.highlightAll();
+    console.timeEnd('Prism.highlightAll');
+  }, 1000);
 });
