3d12bb24e8f6f1704a2f6c78f252233b29d3cc4d
Siavash Safi <siavash.safi@gmail.com>
2020-10-31 15:50:10 +0100

    Dynamic event handling based on JIRA version
    Dynamically handle events based on JIRA version.
    - `>= 8.0.0`: use `JIRA.ViewIssueTabs.onTabReady()`
    - `< 8.0.0`: bind `JIRA.Events.NEW_CONTENT_ADDED`

--- src/main/resources/js/event.js
+++ src/main/resources/js/event.js
@@ -1,29 +1,39 @@
-// Highlight new content dynamically for older versions of JIRA
-JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (e, context, reason) {
-  switch(reason) {
-    case "pageLoad":
-    case "panelRefreshed":
-      console.debug("Prism captured event: JIRA.Events.NEW_CONTENT_ADDED, reason:", reason);
+AJS.toInit(function(){
+  if (JIRA.Version.isGreaterThanOrEqualTo("8.0.0")) {
+    console.debug("Prism detected JIRA >= 8.0.0");
+    // Highlight (new) content in issue tabs
+    JIRA.ViewIssueTabs.onTabReady(function() {
+      console.debug("Prism captured event: JIRA.ViewIssueTabs.onTabReady");
+      /* Set a timeout since JIRA navigator fires this event too early,
+        resulting in code style to be lost!
+      */
       setTimeout(function(){
-        console.time('Prism.highlightAllUnder');
-        Prism.highlightAllUnder(e.target);
-        console.timeEnd('Prism.highlightAllUnder');
+        console.time('Prism.highlightAll');
+        Prism.highlightAll();
+        console.timeEnd('Prism.highlightAll');
       }, 1000);
-      break;
-    default:
-      console.debug("Prism ignored reason:", reason)
-  }
-});
+    });
 
-// Highlight (new) content in issue tabs
-JIRA.ViewIssueTabs.onTabReady(function() {
-  /* Set a timeout since JIRA navigator fires this event too early,
-     resulting in code style to be lost!
-  */
-  console.debug("Prism captured event: JIRA.ViewIssueTabs.onTabReady");
-  setTimeout(function(){
-    console.time('Prism.highlightAll');
-    Prism.highlightAll();
-    console.timeEnd('Prism.highlightAll');
-  }, 1000);
+  } else {
+    console.debug("Prism detected JIRA < 8.0.0");
+    // Highlight new content dynamically for older versions of JIRA
+    JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (e, context, reason) {
+      switch(reason) {
+        case "pageLoad":
+        case "panelRefreshed":
+          console.debug("Prism captured event: JIRA.Events.NEW_CONTENT_ADDED, reason:", reason);
+          /* Set a timeout since JIRA navigator fires this event too early,
+            resulting in code style to be lost!
+          */
+          setTimeout(function(){
+            console.time('Prism.highlightAllUnder');
+            Prism.highlightAllUnder(e.target);
+            console.timeEnd('Prism.highlightAllUnder');
+          }, 1000);
+          break;
+        default:
+          console.debug("Prism ignored reason:", reason)
+      }
+    });
+  }
 });
